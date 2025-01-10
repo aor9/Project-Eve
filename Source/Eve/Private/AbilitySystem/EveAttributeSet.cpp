e6508b3419f28eb64006a963175875d6a6990f75
aor9 <lee612623@gmail.com>
2025-01-10 16:05:54 +0900

    feat: add attribute set
    add attribute set health, body temperature, hunger, stamina

--- Source/Eve/Private/AbilitySystem/EveAttributeSet.cpp
+++ Source/Eve/Private/AbilitySystem/EveAttributeSet.cpp
@@ -3,6 +3,73 @@
 
 #include "AbilitySystem/EveAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
 
+UEveAttributeSet::UEveAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitBodyTemperature(36.5f);
+	InitMaxBodyTemperature(42.f);
+	InitHunger(100.f);
+	InitMaxHunger(100.f);
+	
+	InitStamina(100.f);
+	InitMaxStamina(100.f);
+}
 
+void UEveAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, BodyTemperature, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, MaxBodyTemperature, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, Hunger, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEveAttributeSet, MaxHunger, COND_None, REPNOTIFY_Always);
+}
+
+void UEveAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, Health, OldHealth);
+}
+
+void UEveAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UEveAttributeSet::OnRep_BodyTemperature(const FGameplayAttributeData& OldBodyTemperature) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, BodyTemperature, OldBodyTemperature);
+}
+
+void UEveAttributeSet::OnRep_MaxBodyTemperature(const FGameplayAttributeData& OldMaxBodyTemperature) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, MaxBodyTemperature, OldMaxBodyTemperature);
+}
+
+void UEveAttributeSet::OnRep_Hunger(const FGameplayAttributeData& OldHunger) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, Hunger, OldHunger);
+}
+
+void UEveAttributeSet::OnRep_MaxHunger(const FGameplayAttributeData& OldMaxHunger) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, MaxHunger, OldMaxHunger);
+}
+
+void UEveAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, Stamina, OldStamina);
+}
+
+void UEveAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEveAttributeSet, MaxStamina, OldMaxStamina);
+}
--- Source/Eve/Public/AbilitySystem/EveAttributeSet.h
+++ Source/Eve/Public/AbilitySystem/EveAttributeSet.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "EveAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,7 +21,71 @@ class EVE_API UEveAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UEveAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// Vital Attributes
+	// Health
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_Health", Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxHealth", Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, MaxHealth);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	// Body Temperature
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_BodyTemperature", Category = "Vital Attributes")
+	FGameplayAttributeData BodyTemperature;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, BodyTemperature);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxBodyTemperature", Category = "Vital Attributes")
+	FGameplayAttributeData MaxBodyTemperature;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, MaxBodyTemperature);
+
+	UFUNCTION()
+	void OnRep_BodyTemperature(const FGameplayAttributeData& OldBodyTemperature) const;
 	
+	UFUNCTION()
+	void OnRep_MaxBodyTemperature(const FGameplayAttributeData& OldMaxBodyTemperature) const;
+
+	// Hunger
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_Hunger", Category = "Vital Attributes")
+	FGameplayAttributeData Hunger;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, Hunger);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxHunger", Category = "Vital Attributes")
+	FGameplayAttributeData MaxHunger;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, MaxHunger);
+
+	UFUNCTION()
+	void OnRep_Hunger(const FGameplayAttributeData& OldHunger) const;
 	
+	UFUNCTION()
+	void OnRep_MaxHunger(const FGameplayAttributeData& OldMaxHunger) const;
+
+
+	// Combat Attributes
+	// Stamina
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_Stamina", Category = "Combat Attributes")
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, Stamina);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxStamina", Category = "Combat Attributes")
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(UEveAttributeSet, MaxStamina);
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+	
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 	
 };
